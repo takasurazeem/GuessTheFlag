c1adb1c13aaf62f08bfe9b635128796a2b4e7834
Takasur Azeem <takasurazeem@gmail.com>
2022-02-19 18:26:49 +0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Takasur Azeem on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
