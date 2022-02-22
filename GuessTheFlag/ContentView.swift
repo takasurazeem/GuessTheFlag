f23f4c488ffd8bc76e281ecf9adb31e573e09e38
Takasur Azeem <takasurazeem@gmail.com>
2022-02-22 18:42:18 +0500

    complete the challenge task
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-//    @State private var showingScore = false
+    let maxNumberOfQuestions = 8
+    @State private var showingScore = false
     @State private var scroreTitle = ""
     @State private var score = 0
+    @State private var lastClicked = 0
+    @State private var numberOfQuestionsAsked = 0
+    @State private var showEndGameAlert = false
     
     @State private var countries = ["Pakistan", "Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -62,28 +66,47 @@ struct ContentView: View {
             .padding()
             
         }
-//        .alert(scroreTitle, isPresented: $showingScore) {
-//            Button("Continue", action: askQuestion)
-//        } message: {
-//            Text("Your score is \(score)")
-//        }
+        .alert(scroreTitle, isPresented: $showingScore) {
+            Button("Continue") {
+                askQuestion()
+            }
+        } message: {
+            Text("You clicked flag of \(countries[lastClicked]).\nScore penalty -1")
+            
+        }
+        .alert("Your final score is \(score)/\(maxNumberOfQuestions)", isPresented: $showEndGameAlert) {
+            Button("Reset") {
+                resetGame()
+            }
+        }
+    }
+    
+    func resetGame() {
+        score = 0
+        lastClicked = 0
+        numberOfQuestionsAsked = 0
     }
     
     func flagTapped(_ number: Int) {
+        lastClicked = number
         if number == correctAnswer {
             scroreTitle = "Correct"
             score += 1
+            askQuestion()
         } else {
+            showingScore = true
             scroreTitle = "Wrong"
             score -= 1
         }
-        askQuestion()
-//        showingScore = true
     }
     
     func askQuestion() {
+        correctAnswer = Int.random(in: 0...2)
         countries.shuffle()
-        correctAnswer = Int.random(in: 0...02)
+        numberOfQuestionsAsked += 1
+        if numberOfQuestionsAsked == 8 {
+            showEndGameAlert = true
+        }
     }
 }
 
